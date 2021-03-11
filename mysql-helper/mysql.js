3390faaa5cbcbf28e261ffdae7697369151ffdf0
Dev PI <thermosensepi@thermally.ca>
2021-03-11 11:47:45 -0500

    mysql init

---
+++ mysql-helper/mysql.js
@@ -0,0 +1,9 @@
+var mysql = require('mysql');
+var cfg = require('./mysql-cfg')
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : cfg.host,
+  user            : cfg.username,
+  password        : cfg.password,
+  database        : 'my_db'
+});
