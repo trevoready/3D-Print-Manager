90e695ec7971cecd4777ef10cca84de5e58176f8
Dev PI <thermosensepi@thermally.ca>
2021-03-14 17:12:54 -0400

    remove unused import

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const fileUpload = require('express-fileupload');
 var fs = require('fs');
