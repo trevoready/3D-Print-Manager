fa5e67e5ad741f3d1080dc08b59c96a21852d878
Dev PI <thermosensepi@thermally.ca>
2021-03-14 22:34:05 -0400

    add job to db add order and filament ids

--- routes/jobs.js
+++ routes/jobs.js
@@ -8,18 +8,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Jobs' });
 });
 router.get('/add', function(req, res, next) {
-  pool.query('SELECT * FROM files', function (error, results, fields) {
+  pool.query('SELECT * FROM files', function (error, resultsFiles, fields) {
     if (error) console.log(error);
-    res.render('add-job', { files: results,uid:uid.sync(8)});
+    pool.query('SELECT * FROM orders', function (error, resultsOrders, fields) {
+      if (error) console.log(error);
+      pool.query('SELECT * FROM filament', function (error, resultsFilament, fields) {
+        if (error) console.log(error);
+        res.render('add-job', { files: resultsFiles,orders: resultsOrders,filaments: resultsFilament,uid:uid.sync(8)});
+      })
+    })
   })
-});router.post('/new', function(req, res, next) {
+});
+router.post('/new', function(req, res, next) {
   console.log(req.body)
   if(req.body.fileID == ''){
     return res.redirect('/jobs/add')
   }
-  pool.query('SELECT * FROM files WHERE ID=?',[req.body.id], function (error, results, fields) {
-    if (error) console.log(error);
-    res.redirect('/jobs')
+  var form = req.body
+  pool.query('SELECT * FROM files WHERE ID=?',[form.fileID], function (error, results, fields) {
+    pool.query('INSERT INTO jobs (barcode,fileID,filamentID,estPrintTime,orderID) VALUES(?,?,?,?,?)',[form.jobCode,form.fileID,form.filamentID,results[0].time,form.orderID], function (error, results, fields) {
+      if (error) console.log(error);
+      res.redirect('/jobs')
+    })
   })
 });
 
