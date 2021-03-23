a34b2ca1cbb2f1341256dcb4e1317b5dd8a2acad
Dev PI <thermosensepi@thermally.ca>
2021-03-23 00:19:09 -0400

    add filament list page

--- routes/filament.js
+++ routes/filament.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var pool = require('../mysql-helper/mysql')
 //Filament Rolls
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Filament' });
+router.get('/', async function(req, res, next) 
+{
+  let rolls = await pool.query('SELECT * FROM filament') 
+  res.render('filament', { rolls: rolls });
 });
 
 module.exports = router;
