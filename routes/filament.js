9c74bec1706f07b8942b85d27a98de2df5b78f27
Trevor Eady <trevor.eady@thermally.ca>
2021-03-23 22:20:05 -0400

    add filament roll

--- routes/filament.js
+++ routes/filament.js
@@ -1,18 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var pool = require('../mysql-helper/mysql')
-//Filament Rolls
-router.get('/', async function(req, res, next) 
-{
-  let rolls = await pool.query('SELECT * FROM filament') 
-  res.render('filament', { rolls: rolls });
+    //Filament Rolls
+router.get('/', async function(req, res, next) {
+    let rolls = await pool.query('SELECT * FROM filament')
+    res.render('filament', { rolls: rolls });
 });
-router.get('/add', async function(req, res, next) 
-{ 
-  let colors = await pool.query('SELECT * FROM colors') 
-  let materials = await pool.query('SELECT * FROM materials') 
-  let vendors = await pool.query('SELECT * FROM vendors') 
-  res.render('add-filament',{vendors:vendors,materials:materials,colors:colors});
+router.get('/add', async function(req, res, next) {
+    let colors = await pool.query('SELECT * FROM colors')
+    let materials = await pool.query('SELECT * FROM materials')
+    let vendors = await pool.query('SELECT * FROM vendors')
+    res.render('add-filament', { vendors: vendors, materials: materials, colors: colors });
+});
+router.post('/add', async function(req, res, next) {
+    if (req.body.barcode) {
+        let data = req.body;
+        await pool.query('INSERT INTO filament (barcode,material,spoolAmount,amountRemaining,purchasePrice,color,purchaseDate,vendor) VALUES(?,?,?,?,?,?,?,?)', [data.barcode, data.material, data.amount, data.currentAmount, data.price, data.color, data.purchaseDate, data.vendor])
+    }
+    res.redirect('/filament');
 });
 
 module.exports = router;
