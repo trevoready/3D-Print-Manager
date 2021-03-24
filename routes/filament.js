123586e0811b4bfef9506381cda30899458f045c
Dev PI <thermosensepi@thermally.ca>
2021-03-23 21:35:04 -0400

    add filament page

--- routes/filament.js
+++ routes/filament.js
@@ -7,5 +7,12 @@ router.get('/', async function(req, res, next)
   let rolls = await pool.query('SELECT * FROM filament') 
   res.render('filament', { rolls: rolls });
 });
+router.get('/add', async function(req, res, next) 
+{ 
+  let colors = await pool.query('SELECT * FROM colors') 
+  let materials = await pool.query('SELECT * FROM materials') 
+  let vendors = await pool.query('SELECT * FROM vendors') 
+  res.render('add-filament',{vendors:vendors,materials:materials,colors:colors});
+});
 
 module.exports = router;
