b7a306aa6600ed87a58c8ee9375cb05c575b137c
Trevor Eady <trevor.eady@thermally.ca>
2021-04-15 11:10:37 -0400

    Added orders pages

--- app.js
+++ app.js
@@ -11,19 +11,20 @@ var files = require('./routes/files');
 var prints = require('./routes/prints');
 var printers = require('./routes/printers');
 var jobs = require('./routes/jobs');
+var orders = require('./routes/orders');
 var session = require('express-session')
 var app = express();
 app.set('trust proxy', 1) // trust first proxy
 app.use(session({
-  secret: 'Change This',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: true }
+    secret: 'Change This',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
 }))
 app.use(fileUpload({
-  useTempFiles : true,
-  tempFileDir : path.join(__dirname, 'uploads'),
-  debug:true
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, 'uploads'),
+    debug: true
 }));
 
 // view engine setup
@@ -40,43 +41,44 @@ app.use('/filament', filament);
 app.use('/files', files);
 app.use('/jobs', jobs);
 app.use('/printers', printers);
+app.use('/orders', orders);
 app.use('/', index);
 
 // Make files dir if not exists
-if (!fs.existsSync('./files')){
-  fs.mkdirSync('./files');
-  console.log('Made files Folder')
+if (!fs.existsSync('./files')) {
+    fs.mkdirSync('./files');
+    console.log('Made files Folder')
 }
 //delete files in upload dir(temp fix for issue #3)
 fs.readdir('./uploads', (err, files) => {
-  if (err) throw err;
+    if (err) throw err;
 
-  for (const file of files) {
-    fs.unlink(path.join('./uploads', file), err => {
-      if (err) throw err;
-    });
-  }
+    for (const file of files) {
+        fs.unlink(path.join('./uploads', file), err => {
+            if (err) throw err;
+        });
+    }
 });
 // Make uploads dir if not exists
-if (!fs.existsSync('./uploads')){
-  fs.mkdirSync('./uploads');
-  console.log('Made Uploads Folder')
+if (!fs.existsSync('./uploads')) {
+    fs.mkdirSync('./uploads');
+    console.log('Made Uploads Folder')
 }
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
---
+++ routes/orders.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var uid = require('uid-safe')
+var path = require('path');
+var pool = require('../mysql-helper/mysql.js');
+
+//All Files
+router.get('/', async function(req, res, next) {
+    let orders = await pool.query('SELECT * FROM orders')
+    res.render('orders', { orders: orders });
+});
+router.get('/add', async function(req, res, next) {
+    let customers = await pool.query('SELECT * FROM customers')
+    res.render('add_order', { customers: customers });
+});
+router.post('/new', async function(req, res, next) {
+    res.redirect('/orders/')
+});
+module.exports = router;
